fd69bc7ee4ed3ec68be31531639a2d4fcceec363
Daina Slezaite <daina.slezaite@gmail.com>
2020-12-18 19:59:18 +0100

    Add functionality to remove milestones when creating challenge

--- public/javascripts/logic.js
+++ public/javascripts/logic.js
@@ -2,10 +2,19 @@ const milestoneAdd = document.getElementById('add-milestone');
 const milestoneDiv = document.getElementById('milestones');
 
 milestoneAdd.addEventListener('click', event => {
+  const div = document.createElement('div');
   const textarea = document.createElement('textarea');
   textarea.setAttribute('type', 'text');
   textarea.setAttribute('name', 'milestones');
-  milestoneDiv.appendChild(textarea);
+  const removeButton = document.createElement('button');
+  removeButton.innerHTML="Remove";
+  removeButton.onclick = event => {
+    removeButton.parentNode.parentNode.removeChild(div);
+    event.preventDefault();
+  };
+  div.appendChild(textarea);
+  div.appendChild(removeButton);
+  milestoneDiv.appendChild(div);
   event.preventDefault();
 });
 
