063d187449a1b30a55de6621bbca3f790095e732
Daina Slezaite <daina.slezaite@gmail.com>
2021-01-07 20:00:41 +0100

    Add styling to invite friends view and edit email html

--- routes/email.routes.js
+++ routes/email.routes.js
@@ -1,14 +1,13 @@
-
 const nodemailer = require('nodemailer'); 
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/User.model');
 
-
-
 router.post('/send-email', (req, res, next) => {
-  let { email, subject, message } = req.body;
+  const user = req.session.currentUser;
+  let { email, message } = req.body;
+  let subject = `Invitation for you from ${user.username} - join Challenge Hub today!`;
   let transporter = nodemailer.createTransport({
     service: 'gmail', 
     auth: { 
@@ -16,16 +15,16 @@ router.post('/send-email', (req, res, next) => {
       pass: 'Challengeapp!'
     }
   });
+
   transporter.sendMail({
     from: '"ChallengeHub " <getchallenged42@gmail.com>',
     to: email, 
     subject: subject, 
     text: message,
-    html: `<h1>Here is your Message</h1><p>${message}</p>`
+    html: `<h1>Your invitation</h1><p>${user.username} is inviting you to join Challenge Hub and start working on your goals! Here's their message for you: "${message}"</p><button><a href="https://challenge-hub.herokuapp.com/signup">Accept the invitation</a></button>`
   })
-  .then(info => res.render('index'))
+  .then(() => res.render('invite', {successMessage: 'Invitation sent!'}))
   .catch(error => console.log(error));
 });
-
 
 module.exports = router;
